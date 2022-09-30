7e0e3466dd05f6d4111bf7a04638de28a266ecd5
Manuel Garcia F <mgarciafrigo@gmail.com>
2022-09-30 10:45:22 -0300

    fix TAD list

--- src/api/tads/List.hpp
+++ src/api/tads/List.hpp
@@ -1,131 +1,113 @@
 #ifndef _TLIST_TAD_
 #define _TLIST_TAD_
 
-#include <iostream>
 #include "../funciones/lists.hpp"
+#include <iostream>
 
 using namespace std;
 
-template <typename T>
-struct List
+template <typename T> struct List
 {
-   Node<T> *node;
-   Node<T> *iterator;
+  Node<T> *node;
+  Node<T> *iterator;
 };
 
-template <typename T>
-List<T> list()
+template <typename T> List<T> list()
 {
-   List<T> lst;
-   lst.node = NULL;
-   return lst;
+  List<T> lst;
+  lst.node = NULL;
+  return lst;
 }
 
-template <typename T>
-T *listAdd(List<T> &lst, T e)
+template <typename T> T *listAdd(List<T> &lst, T e)
 {
-   Node<T> *element = add<T>(lst.node, e);
-   return &(element->next);
+  Node<T> *element = add<T>(lst.node, e);
+  return &(element->info);
 }
 
-template <typename T>
-T *listAddFirst(List<T> &lst, T e)
+template <typename T> T *listAddFirst(List<T> &lst, T e)
 {
-   Node<T> *element = addFirst<T>(lst.node, e);
-   return &(element->next);
+  Node<T> *element = addFirst<T>(lst.node, e);
+  return &(element->info);
 }
 
 template <typename T, typename K>
 T listRemove(List<T> &lst, K k, int cmpTK(T, K))
 {
-   T element = remove<T, K>(lst.node, k, cmpTK);
-   return element;
+  T element = remove<T, K>(lst.node, k, cmpTK);
+  return element;
 }
 
-template <typename T>
-T listRemoveFirst(List<T> &lst)
+template <typename T> T listRemoveFirst(List<T> &lst)
 {
-   return removeFirst<T>(lst.node);
+  return removeFirst<T>(lst.node);
 }
 
-template <typename T, typename K>
-T *listFind(List<T> lst, K k, int cmpTK(T, K))
+template <typename T, typename K> T *listFind(List<T> lst, K k, int cmpTK(T, K))
 {
-   Node<T> *element = find<T, K>(lst.node, k, cmpTK);
-   return &(element->next);
+  Node<T> *element = find<T, K>(lst.node, k, cmpTK);
+  return &(element->next);
 }
 
-template <typename T>
-bool listIsEmpty(List<T> lst)
+template <typename T> bool listIsEmpty(List<T> lst)
 {
-   return isEmpty<T>(lst.node);
+  return isEmpty<T>(lst.node);
 }
 
-template <typename T>
-int listSize(List<T> lst)
+template <typename T> int listSize(List<T> lst)
 {
-   int i = 0;
-   // Count the nodes
-   for (Node<T> *aux = lst.node; aux != NULL; i++)
-   {
-      i++;
-   }
-   return i;
+  int i = 0;
+  // Count the nodes
+  for (Node<T> *aux = lst.node; aux != NULL; i++)
+  {
+    i++;
+  }
+  return i;
 }
 
-template <typename T>
-void listFree(List<T> &lst)
+template <typename T> void listFree(List<T> &lst) { free<T>(lst.node); }
+
+template <typename T> T *listOrderedInsert(List<T> &lst, T t, int cmpTT(T, T))
 {
-   free<T>(lst.node);
+  return &orderedInsert<T>(lst.node, t, cmpTT);
 }
 
-template <typename T>
-T *listOrderedInsert(List<T> &lst, T t, int cmpTT(T, T))
+template <typename T> void listSort(List<T> &lst, int cmpTT(T, T))
 {
-   return &orderedInsert<T>(lst.node, t, cmpTT);
+  sort<T>(lst.node, cmpTT);
 }
 
-template <typename T>
-void listSort(List<T> &lst, int cmpTT(T, T))
+template <typename T> void listReset(List<T> &lst)
 {
-   sort<T>(lst.node, cmpTT);
+  // Reset the iterator to the first node
+  lst.iterator = lst.node;
 }
 
-template <typename T>
-void listReset(List<T> &lst)
+template <typename T> bool listHasNext(List<T> lst)
 {
-   // Reset the iterator to the first node
-   lst.iterator = lst.node;
+  // Check if the iterator is not null
+  return lst.iterator != NULL;
 }
 
-template <typename T>
-bool listHasNext(List<T> lst)
+template <typename T> T *listNext(List<T> &lst)
 {
-   // Check if the iterator is not null
-   return lst.iterator != NULL;
+  // Get the next element
+  lst.iterator = lst.iterator->next;
+  return &(lst.iterator->info);
 }
 
-template <typename T>
-T *listNext(List<T> &lst)
+template <typename T> T *listNext(List<T> &lst, bool &endOfList)
 {
-   // Get the next element
-   lst.iterator = lst.iterator->next;
-   return &(lst.iterator->info);
-}
-
-template <typename T>
-T *listNext(List<T> &lst, bool &endOfList)
-{
-   if (listHasNext(lst))
-   {
-      endOfList = false;
-      return listNext(lst);
-   }
-   else
-   {
-      endOfList = true;
-      return NULL;
-   }
+  if (listHasNext(lst))
+  {
+    endOfList = false;
+    return listNext(lst);
+  }
+  else
+  {
+    endOfList = true;
+    return NULL;
+  }
 }
 
 #endif
